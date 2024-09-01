5e771726911f2cf05b1e8d7c308da917ea3143af
SaiKarthikC-137 <kosurisaikarthik2132@gmail.com>
2024-08-31 20:28:16 -0400

    added optimizations

--- index_test.py
+++ index_test.py
@@ -32,8 +32,8 @@ def initialize_driver(headless=False):
     service = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=service, options=options)
 
-simplify_url = "https://simplify.jobs/jobs?state=United%20States&points=71.5388001%3B-66.885417%3B18.7763%3B-180&country=United%20States&experience=Internship%3BEntry%20Level%2FNew%20Grad%3BJunior%3BMid%20Level&category=Software%20Engineering%3BBackend%20Engineering%3BFull-Stack%20Engineering%3BDevOps%20%26%20Infrastructure%3BSoftware%20Development%20Management%3BFrontend%20Engineering%3BSoftware%20QA%20%26%20Testing%3BDevOps%20Engineering%3BMobile%20Engineering%3BCloud%20Engineering%3BIOS%20Development%3BAndroid%20Development%3BWeb%20Development"
-job_category = "Software Engineer"
+simplify_url = "https://simplify.jobs/jobs?state=United%20States&points=71.5388001%3B-66.885417%3B18.7763%3B-180&country=United%20States&experience=Internship%3BEntry%20Level%2FNew%20Grad%3BJunior%3BMid%20Level&category=Operations%20%26%20Logistics%3BCustomer%20Success%20Management%3BIT%20%26%20Security%3BCustomer%20Success%20%26%20Support%3BMechanical%20Engineering%3BMedical%2C%20Clinical%20%26%20Veterinary%3BEngineering%20Management%3BLegal%20%26%20Compliance%3BElectrical%20Engineering%3BSupply%20Chain%20Management%3BPeople%20%26%20HR%3BCybersecurity%3BCustomer%20Support%3BConsulting%3BHuman%20Resources%3BContent%20%26%20Writing%3BStrategy%20Development%3BSolution%20Engineering%3BLegal%3BManagement%20Consulting%3BAerospace%20Engineering%3BManufacturing%20Engineering%3BBiology%20%26%20Biotech%3BHealthcare%20Administration%20%26%20Support%3BEmbedded%20Engineering%3BElectronics%20Design%20Engineering%3BArchitecture%20%26%20Civil%20Engineering%3BAdministrative%20%26%20Executive%20Assistance%3BMechanical%20Maintenance%20and%20Reliability%20Engineering%3BLab%20%26%20Research%3BContent%20Strategy%3BNursing%20%26%20Allied%20Health%20Professionals%3BIT%20Support%3BRisk%20%26%20Compliance%3BCustomer%20Success%3BReal%20Estate%3BAdministrative%20Support%3BHardware%20Engineering%3BDesign%20Engineering%3BProcurement%20%26%20Sourcing%3BBiology%20Lab%20%26%20Research%3BResearch%20%26%20Development%3BSite%20Reliability%20Engineering%3BIT%20Project%20Management%3BLife%20Sciences%3BPhysicians%20%26%20Surgeons%3BRisk%20Management%3BPR%20%26%20Communications%3BIT%20%26%20Support%3BIT%20Consulting%3BSystems%20Engineering%20Management%3BGame%20Engineering%3BTechnical%20Writing%3BConstruction%20Management%3BPower%20Systems%20Engineering%3BSystem%20Administration%3BInventory%20Management%3BPlatform%20Engineering%3BSecurity%20Engineering%3BInsurance%3BRobotics%20and%20Automation%20Engineering%3BCopywriting%3BContract%20Management%3BNetwork%20Administration%3BControl%20Systems%20Engineering%3BVeterinary%20Professionals%3BAuditing%3BTechnical%20Recruiting%3BQuantitative%20Research%3BExecutive%20Support%3BWarehouse%20Operations%3BJournalism%3BPublic%20Health%3BThermal%20and%20Fluid%20Systems%20Engineering%3BElectronic%20Hardware%20Engineering%3BInstrumentation%20and%20Measurement%20Engineering%3BTransportation%20Engineering%3BMaterials%20%26%20Structures%3BEmbedded%20Systems%20Engineering%3BComputational%20Biology%3BStructural%20Engineering%3BHardware%20Validation%20%26%20Testing%3BCreative%20Writing%3BDigital%20Journalism%3BPropulsion%3BBioinformatics%3BEnvironmental%20Engineering%3BQuantitative%20Analysis%3BBookkeeping%3BRF%20and%20Microwave%20Engineering%3BRobotics%20%26%20Autonomous%20Systems%3BAlgorithm%20Development%3BEditing%20%26%20Proofreading%3BMedical%20Research%3BEnvironmental%20Sciences%3BAutomation%20Testing%3BSpace%20%26%20Rocket%20Engineering%3BComputer%20Hardware%20Engineering%3BAvionics%3BCustomer%20Experience%3BArchitectural%20Design%3BDiagnostics%20%26%20Laboratory%20Professionals%3BInterdisciplinary%20Research%3BGenomics%3BCustomer%20Education%20%26%20Training%3BFlight%20Dynamics%3BNetwork%20Engineering%3BTreasury%3BPrivate%20Equity%3BStrategy%20Consulting%3BSystem%20Hardware%20Engineering%3BSpecialized%20Administrative%20Roles%3BManagement%20Accounting%3BEnvironmental%20Consulting%3BCost%20Accounting%3BWater%20Resources%20Engineering%3BSecurity%20Engineering%20Management%3BOperations%20Consulting%3BCopywriting%20Marketing%3BIntellectual%20Property%20(IP)%3BBuilding%20Services%20Engineering%3BStrategy%20Research%3BTelecommunications%20Engineering%3BAutomotive%20Electrical%20Engineering%3BNews%20Reporting%3BGeotechnical%20Engineering%3BMicroelectronics%20Engineering%3BSignal%20Processing%20Engineering%3BMechanical%20Hardware%20Engineering%3BTransportation%20%26%20Distribution%3BCustomer%20Service%20%26%20Reception%3BVenture%20Capital%3BAdministration%3BPerformance%20Testing%3BInternational%20Accounting%3BAerodynamics%3BServer%20Administration%3BGeneralist%20Recruiting%3BInvestigative%20Journalism%3BPhysical%20Sciences%3BReal%20Estate%20Marketing%3BUrban%20Planning%3BLegal%20Consulting%3BIndustrial%20Design%3BForensic%20Accounting%3BBroadcast%20Journalism%3BSecurity%20Testing%3BGovernmental%20Accounting%3BStoryboarding%3BTreasury%20Accounting%3BFeature%20Writing%3BEnvironmental%20Accounting%3BScheduling%20%26%20Travel%20Coordination"
+job_category = "Other"
 
 # Initialize driver in non-headless mode for login
 driver = initialize_driver(headless=False)
@@ -45,7 +45,7 @@ def wait_for_element(by, value, timeout=5):
         element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((by, value)))
         return element
     except Exception as e:
-        print(f"Error waiting for element {value}: {e}")
+        print(f"Error waiting for element {value}")
         return None
 
 # Function to login
@@ -150,6 +150,7 @@ def extract_job_details():
 
     except Exception as e:
         print(f"Failed to extract job details")
+        return job_details
     
     return job_details
 
@@ -158,10 +159,11 @@ def apply_for_job(first_job=False):
     retries = 5
     apply_button_clicked = False
     job_details = extract_job_details()
-
+    if len(job_details)==0:
+        return job_details
     for attempt in range(retries):
         try:
-            apply_button = WebDriverWait(driver, 10).until(
+            apply_button = WebDriverWait(driver, 15).until(
                 EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Apply')]"))
             )
             print(f"Apply button found. Attempting to click (Attempt {attempt + 1}).")
@@ -172,13 +174,13 @@ def apply_for_job(first_job=False):
 
             if first_job:
                 try:
-                    checkbox = WebDriverWait(driver, 20).until(
+                    checkbox = WebDriverWait(driver, 15).until(
                         EC.element_to_be_clickable((By.ID, "preventShow"))
                     )
                     checkbox.click()
                     print("Checked 'Don't show this popup again'.")
 
-                    proceed_button = WebDriverWait(driver, 20).until(
+                    proceed_button = WebDriverWait(driver, 15).until(
                         EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Proceed anyway')]"))
                     )
                     proceed_button.click()
@@ -198,7 +200,7 @@ def apply_for_job(first_job=False):
             print(f"Failed to click the Apply button on attempt {attempt + 1}")
 
     if not apply_button_clicked or len(driver.window_handles) == 1:
-        print("Failed to open new tab after 3 attempts. Skipping this job.")
+        print("Failed to open new tab after 5 attempts. Skipping this job.")
         return
 
     driver.switch_to.window(driver.window_handles[1])
@@ -268,10 +270,13 @@ def process_all_jobs():
             time.sleep(3)
 
             job_details = apply_for_job(first_job=(index == 0))
+            if isinstance(job_details,type(None)) or len(job_details)==0:
+                print(f"Skipping job {index + 1} of {len(job_cards)}")
+                continue
             job_data.append(job_details)
 
         except Exception as e:
-            print(f"Error processing job {index + 1}: {e}")
+            print(f"Error processing job {index + 1}")
             time.sleep(2)
 
     job_df = pd.DataFrame(job_data, columns = columns)
